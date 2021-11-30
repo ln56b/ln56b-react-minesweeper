ab844016f6f5274e444b0516bab8511bfd03df48
helene-bleexo <helene.baudrand@bleexo.com>
2021-11-30 20:07:50 +0100

    feat(form): initialise basic form

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -4,6 +4,14 @@ function Editor() {
 	return (
 		<div>
 			<p>Editor component works</p>
+			<div>
+				<form noValidate autoComplete='off'>
+					<input name='rows' type='text' />
+					<input name='cols' type='text' />
+					<input name='mines' type='text' />
+					<button>Submit</button>
+				</form>
+			</div>
 		</div>
 	)
 }
