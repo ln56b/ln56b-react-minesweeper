3099d46893ac71147e37e6f913641a755d1b56b4
Nora <nora@guidap.co>
2021-12-13 20:12:46 +0100

    mines prop owned by Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,25 @@
 import React from "react"
 import Table from "./Table"
 
+const height = 6
+const width = 9
+const testMineNumber = 10
+
 function Layout() {
+	const baseArray = Array(width).fill(Array(height).fill(false))
+  const testMines = JSON.parse(JSON.stringify(baseArray))
+  testMines[0][0] = true
+	const [mineNumber, setMineNumber] = React.useState(testMineNumber)
+	const [mines, setMines] = React.useState(testMines)
+
 	return (
 		<div>
-			<p>Layout component works</p>
-			<Table width={4} height={3}/>
+			<Table
+				width={width}
+				height={height}
+				mineNumber={mineNumber}
+				mines={mines}
+			/>
 		</div>
 	)
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,35 +1,45 @@
 import './Table.css'
-import React from "react"
-import Square from "./Square"
+import React from 'react'
+import Square from './Square'
 
-function Table({ height, width }) {
-  const baseArray = Array(width).fill(Array(height).fill(false))
-  const testMines = JSON.parse(JSON.stringify(baseArray))
-  testMines[0][0] = true
-  const [openFields, setOpenFields] = React.useState(baseArray)
-  const [mines, setMines] = React.useState(testMines)
+function Table({ height, width, mineNumber, mines }) {
+  const baseFieldArray = Array(width).fill(Array(height).fill(false))
+  const baseValueArray = Array(width).fill(Array(height).fill(0))
+
+  const [openFields, setOpenFields] = React.useState(baseFieldArray)
+  const [fieldValues, setFieldValues] = React.useState(baseValueArray)
+
+  React.useEffect(() => {
+    setOpenFields(baseFieldArray)
+    setFieldValues(generateNewFieldValues(mines))
+  }, [mines])
+
+  function generateNewFieldValues(newMines) {
+    // TODO: calculate all the values based on the new location of the mines
+    return baseValueArray
+  }
 
   function openField(x,y) {
     if (openFields[x][y]) {
-      return;
+      return
     }
-    const newOpenFields = JSON.parse(JSON.stringify(openFields));
-    newOpenFields[x][y] = true;
-    setOpenFields(newOpenFields);
+    const newOpenFields = JSON.parse(JSON.stringify(openFields))
+    newOpenFields[x][y] = true
+    setOpenFields(newOpenFields)
   }
 
 	return (
 		<div className="table">
 			{
-        openFields.map((column, x) => (
+        baseFieldArray.map((column, x) => (
           <div className="table-column" key={x}>
             {
-              column.map((isFieldOpen, y) => (
+              column.map((field, y) => (
                 <Square
                   key={`${x.toString()},${y.toString()}`}
-                  value={`${x.toString()},${y.toString()}`}
+                  value={fieldValues[x][y]}
                   isMine={mines[x][y]}
-                  isOpen={isFieldOpen}
+                  isOpen={openFields[x][y]}
                   setIsOpen={() => openField(x, y)}
                 />
               ))
