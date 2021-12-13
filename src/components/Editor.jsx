adb90d8566fd6b7c32a862ad60d06f984fac1da2
helene-bleexo <helene.baudrand@bleexo.com>
2021-12-13 20:12:31 +0100

    feat(in progress ): form
    radio not checking

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,17 +1,131 @@
-import React from "react"
+import React, { useReducer, useState } from "react"
+
+const formReducer = (state, event) => {
+	if (event.reset) {
+		return {
+			apple: "",
+			count: 0,
+			name: "",
+			"gift-wrap": false
+		}
+	}
+	return {
+		...state,
+		[event.name]: event.value
+	}
+}
 
 function Editor() {
+	const [formData, setFormData] = useReducer(formReducer, {
+		count: 100
+	})
+	const [level, setLevel] = useState("beginner")
+
+	const [submitting, setSubmitting] = useState(false)
+
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		setSubmitting(true)
+
+		setTimeout(() => {
+			setSubmitting(false)
+			setFormData({
+				reset: true
+			})
+		}, 3000)
+	}
+
+	const handleChange = (event) => {
+		const isCheckbox = event.target.type === "checkbox"
+		setFormData({
+			name: event.target.name,
+			value: isCheckbox ? event.target.checked : event.target.value
+		})
+	}
+
+	const onLevelChange = (event) => {
+		this.setState({
+			selectedOption: event.target.value
+		})
+	}
+
 	return (
-		<div>
-			<p>Editor component works</p>
-			<div>
-				<form noValidate autoComplete='off'>
-					<input name='rows' type='text' />
-					<input name='cols' type='text' />
-					<input name='mines' type='text' />
-					<button>Submit</button>
-				</form>
-			</div>
+		<div className='wrapper'>
+			<h1>Customize the board</h1>
+			{submitting && (
+				<div>
+					You are submitting the following:
+					<ul>
+						{Object.entries(formData).map(([name, value]) => (
+							<li key={name}>
+								<strong>{name}</strong>: {value.toString()}
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
+			<form onSubmit={handleSubmit}>
+				<fieldset disabled={submitting}>
+					<label>
+						<p>Name</p>
+						<input
+							name='name'
+							onChange={handleChange}
+							value={formData.name || ""}
+						/>
+					</label>
+				</fieldset>
+				<fieldset disabled={submitting}>
+					<div className='radio'>
+						<label>
+							<input
+								type='radio'
+								value='beginner'
+								checked={level === formData.beginner}
+								onChange={onLevelChange}
+							/>
+							Beginner
+						</label>
+					</div>
+
+					<div className='radio'>
+						<label>
+							<input
+								type='radio'
+								value='intermediary'
+								checked={level === formData.intermediary}
+								onChange={onLevelChange}
+							/>
+							Intermediary
+						</label>
+					</div>
+					<div className='radio'>
+						<label>
+							<input
+								type='radio'
+								value='expert'
+								checked={level === formData.expert}
+								onChange={onLevelChange}
+							/>
+							Expert
+						</label>
+					</div>
+					<div className='radio'>
+						<label>
+							<input
+								type='radio'
+								value='custom'
+								checked={level === formData.custom}
+								onChange={onLevelChange}
+							/>
+							Expert
+						</label>
+					</div>
+				</fieldset>
+				<button type='submit' disabled={submitting}>
+					Submit
+				</button>
+			</form>
 		</div>
 	)
 }
