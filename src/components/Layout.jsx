8c97560ce308b83f516f77ce06a78445400fd844
Nora <nora@guidap.co>
2021-12-20 21:04:57 +0100

    randomize mine placement

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,14 +6,30 @@ const width = 9
 const testMineNumber = 10
 
 const createFilledTable = (width, height, value) => Array(width).fill(null).map(() => new Array(height).fill(value))
+const randomInt = (n) => Math.floor(Math.random() * n)
+
+const generateRandomMines = (width, height, mineNumber) => {
+	let mines = createFilledTable(width, height, false)
+	if (mineNumber === 0) {
+		return mines
+	}
+
+	let num = 0
+	do {
+		const x = randomInt(width)
+		const y = randomInt(height)
+		if (!mines[x][y]) {
+			mines[x][y] = true
+			num++
+		}
+	} while (num < mineNumber)
+	return mines
+}
 
 function Layout() {
-  const testMines = createFilledTable(width, height, false)
-  testMines[0][0] = true
-	testMines[3][4] = true
-	testMines[8][2] = true
-	testMines[2][1] = true
  	const [mineNumber, setMineNumber] = React.useState(testMineNumber)
+	
+	const testMines = generateRandomMines(width, height, mineNumber)
 	const [mines, setMines] = React.useState(testMines)
 
 	return (
