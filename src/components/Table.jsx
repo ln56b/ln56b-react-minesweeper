33c87a7ed0530c76c3424e9e5f05ab53d2f62d66
Nora <nora@guidap.co>
2022-01-31 21:06:14 +0100

    open neighbors if field is empty

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -29,6 +29,21 @@ const calculateFieldValues = (width, height, mines) => {
   return fieldValues
 }
 
+const openArea = ({ openFields, fieldValues, x, y }) => {
+  if (x < 0 || openFields.length <= x || y < 0 || openFields[0].length <= y || openFields[x][y]) {
+    return;
+  }
+  openFields[x][y] = true;
+  if (fieldValues[x][y] !== 0) {
+    return openFields;
+  }
+  for (let x1 of [x-1, x, x+1]) {
+    for (let y1 of [y-1, y, y+1]) {
+      openArea({ openFields, fieldValues, x: x1, y: y1 });
+    }
+  }
+}
+
 function Table({ width, height, mineNumber, mines }) {
   const [openFields, setOpenFields] = React.useState(createFilledTable(width, height, false))
   const [fieldValues, setFieldValues] = React.useState(createFilledTable(width, height, 0))
@@ -39,12 +54,13 @@ function Table({ width, height, mineNumber, mines }) {
   }, [mines, height, width])
 
   function openField(x,y) {
-    if (openFields[x][y]) {
-      return
+    let newOpenFields = JSON.parse(JSON.stringify(openFields))
+    if (mines[x][y]) {
+      newOpenFields[x][y] = true;
+    } else {
+      openArea({ openFields: newOpenFields, fieldValues, x, y });
     }
-    const newOpenFields = JSON.parse(JSON.stringify(openFields))
-    newOpenFields[x][y] = true
-    setOpenFields(newOpenFields)
+    setOpenFields(newOpenFields);
   }
 
 	return (
